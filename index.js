d58663551d265bf856a8d4bb4f984020a44ab62b
WonWonGit <bkn020612@gmail.com>
2021-05-11 21:20:44 +0900

    api connect

---
+++ index.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector(".track_btn");
+const select = document.querySelector(".delivery_js");
+
+function getState(track_id,carrier_id){
+    fetch(`https://apis.tracker.delivery/carriers/${carrier_id}/tracks/${track_id}`)
+    .then(
+        function(response){
+            return response.json()
+        }
+    ).then(function(json){
+        
+    })
+}
+
+function getTrackId(){
+    const track_id = document.querySelector(".track_id").value;
+    const carrier_id = select.options[select.selectedIndex].value;
+    console.log(track_id, carrier_id);
+    getState(track_id,carrier_id);
+}
+
+btn.addEventListener("click",getTrackId);
