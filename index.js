f305bec92922cfe6b9e0fcc71b4236dc253f9f5e
WonWonGit <bkn020612@gmail.com>
2021-05-14 23:13:17 +0900

    json table로 출력하기

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const btn = document.querySelector(".track_btn");
 const select = document.querySelector(".delivery_js");
 const list = document.querySelector(".listUl");
-
-let proList = [];
-
+const currentState = document.querySelector(".currentState");
 
 function getState(track_id,carrier_id){
     fetch(`https://apis.tracker.delivery/carriers/${carrier_id}/tracks/${track_id}`)
@@ -13,27 +11,55 @@ function getState(track_id,carrier_id){
         }
     ).then(function(json){
         loadInfo(json)
+        currentInfo(json)
     })
 }
 
 function loadInfo(json){
     let length = json.progresses.length;
     let prog = json.progresses;
+    const weeks = new Array('SUN','MON','TUE','WED','THU','FRI','SAT');
         for(let i=length-1;i>0;i--){
-            const li = document.createElement("li");
-            const span_time = document.createElement("span");
-            const span_status = document.createElement("span");
-            var tiem = new Date(prog[i]['time']);
-            li.setAttribute("id",`${length-i}`);
-            span_time.setAttribute("id","time");
-            span_status.setAttribute("id","statuse")
-            li.appendChild(span_time);
-            li.appendChild(span_status);
-            /*span_time.textContent= time.slice(0,19);*/
-            /*시간 파싱 연구하기*/
-            span_status.textContent=prog[i]['status']['text'];
-            list.appendChild(li);
+            var time = prog[i]['time'];
+            var state = prog[i]['status']['text'];
+            var location =prog[i]['location']['name'];
+            
+            var tdTime = document.createTextNode(time);
+            var tdState = document.createTextNode(state);
+            var tdLocation = document.createTextNode(location);
+
+
+            const date = new Date(time);
+            const year = date.getFullYear();
+            const month = date.getMonth();
+            const date_t = date.getDate();
+            const day = date.getDay();
+            const hour = date.getHours();
+            const min = date.getMinutes();
+
+            tdTime.textContent = `${year}.${month<10 ? `0${month}` : month}.${date_t < 10 ? `0${date_t}` : date_t} ${weeks[day]} ${hour < 10 ? `0${hour}`: hour}:${min < 10 ? `0${min}` : min}`;
+
+            var tr = document.createElement("tr");
+            var td_time = document.createElement("td");
+            var td_status = document.createElement("td");
+            var td_location = document.createElement("td");
+
+            td_time.appendChild(tdTime);
+            td_status.appendChild(tdState);
+            td_location.appendChild(tdLocation);
+
+            tr.appendChild(td_time);
+            tr.appendChild(td_status);
+            tr.appendChild(td_location);
+
+           var table_info = document.getElementsByTagName("tbody")[0];
+           table_info.appendChild(tr);
         }
+}
+
+function currentInfo(json){
+    const from = json.from['address'];
+    const to = json.to['address'];
 }
 
 
